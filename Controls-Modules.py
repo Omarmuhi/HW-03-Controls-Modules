4a1ead56e1e94ee0a501265c0b936784252644ae
omarmuhiyadin <muhiomar9@gmai.com>
2021-07-21 17:37:12 -0700

    Initial commit

---
+++ Controls-Modules.py
@@ -0,0 +1,32 @@
+public: int
+
+
+def largerAbsVal():
+    pass
+
+
+largerAbsVal
+
+
+class Math:
+    pass
+
+
+Math, int
+
+
+class Math:
+    pass
+
+
+Math
+
+
+class Math:
+    @classmethod
+    def max(cls, param, param1):
+        pass
+
+    @classmethod
+    def abs(cls):
+        passreturn: Math.max(Math.abs(), Math.abs())
---
+++ Q 2.py
@@ -0,0 +1,41 @@
+# Write a function  called max_value that takes two random integers as
+# parameters and returns the larger of the two values. A call of max_value(13, 5)
+# would return 13, and a call of max_value(3, -7) would return 3.
+from unittest import result
+
+import a
+import value as value
+from cherrypy.lib import static
+from public import public
+
+public
+
+
+class MaxValue:
+    pass
+
+
+class Test :MaxValue
+public: static void main (String[] args) /*Function call*/
+int
+
+
+class MacValue:
+    pass
+
+
+absValue=MacValue (13,5) ; intMacValue: MaxValue=MaxValue ()
+//
+
+class Display:
+    pass
+
+
+Display result System. out.println ("A call of large rAbsVal (11,2) would return "+absValue)
+System. out.println ("A call of MaxValue (3, -7) would return "+largeAbs)
+{ /*This function returns the MaxValue of * the two absolute values. */ public static int MaxValue
+(
+a,
+b) /Find MaxValue value by using math
+class. */ int large = Math.max (Math.abs (a), Math. abs (b)) ;
+return large
